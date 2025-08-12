dad6a4cda96691a4822d9713cb3bcd8daeecf6ca
Th4lesz <thalesqm2015@gmail.com>
2025-08-12 19:51:41 -0300

    acrescentado else

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -1,7 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int idade = 24;
-Console.WriteLine("A idade é" + idade);
+int idade = 16;
+Console.WriteLine("A idade é " + idade);
 if (idade >= 18)
-{
     Console.WriteLine("você pode tirar habilitação");
-}
+
+else
+    Console.WriteLine("você não pode tirar habilitação");
