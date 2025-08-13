8430d9a5c986c63c2394eef4601e11a3d7c15a39
Th4lesz <thalesqm2015@gmail.com>
2025-08-13 11:56:03 -0300

    dever feito

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -10,17 +10,40 @@
 
 Console.WriteLine("Informe seu salário: ");
 decimal salario = decimal.Parse(Console.ReadLine());
+var Calculo1 = salario * 0.075m;
+var Calculo2 = salario * 0.15m;
+var Calculo3 = salario * 0.225m;
+var Calculo4 = salario * 0.275m;
+
+
 
 if (salario <= 2259.20m)
     Console.WriteLine("você está isento do IRPF");
 
 else if (salario >= 2259.21m && salario <= 2826.65m)
-    Console.WriteLine("sua aliquota é de 7,5% do seu salário");
+{
+    Console.WriteLine($"você terá o valor de R${Calculo1} descontado do seu salário, referente aos 7,5% da alíquota, que ficará {salario} - {Calculo1}");
+    var resultado_calculo1 = salario - Calculo1;
+    Console.WriteLine($"seu salário líquido é {resultado_calculo1}");
+}
 
 else if (salario >= 2826.66m && salario <= 3751.05m)
-    Console.WriteLine("sua aliquota é de 15% do seu salário");
+{
+    Console.WriteLine($"você terá o valor de R${Calculo2} descontado do seu salário, referente aos 15% da alíquota, que ficará {salario} - {Calculo2}");
+    var resultado_calculo2 = salario - Calculo2;
+    Console.WriteLine($"seu salário líquido é {resultado_calculo2}");
+}
 
 else if (salario >= 3751.06m && salario <= 4664.68m)
-    Console.WriteLine("sua aliquota é de 22,5% do seu salário");
+{
+    Console.WriteLine($"Você terá o valor de R${Calculo3} descontado do seu salário, referente aos 22,5% da alíquota, que ficará {salario} - {Calculo3}");
+    var resultado_calculo3 = salario - Calculo3;
+    Console.WriteLine($"seu salário líquido é {resultado_calculo3}");
+}
 
-
+else
+{ 
+    Console.WriteLine($"Você terá o valor de {Calculo4} descontado do seu salário, referente aos 27,5% da alíquota, que ficará {salario} - {Calculo4}");
+    var resultado_calculo4 = salario - Calculo4;
+    Console.WriteLine($"seu salário líquido será {resultado_calculo4}");
+}
