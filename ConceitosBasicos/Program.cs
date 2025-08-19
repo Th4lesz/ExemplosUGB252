1494de1c5ce531e076f6d057a2135b0bb5b43abb
Th4lesz <thalesqm2015@gmail.com>
2025-08-19 20:09:03 -0300

    bloco try catch

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -1,49 +1,35 @@
-﻿//Console.WriteLine("Informe seu nome: ");
-//string nome = Console.ReadLine();
-//Console.WriteLine("informe seu salário: ");
-//decimal salario = decimal.Parse(Console.ReadLine());
+﻿//informem o valor do IRPF a ser pago de acordo com o salário informado. Utilize a tabela real de desconto do IRPF.
 
-//Console.WriteLine("seu nome é: " + nome);
-//Console.WriteLine("seu salario é: " + salario);
-
-//informe o valor do IRPF a ser pago de acordo com o salário informado. Utilize a tabela real de desconto de IRPF
-
-Console.WriteLine("Informe seu salário: ");
-decimal salario = decimal.Parse(Console.ReadLine());
-var Calculo1 = salario * 0.075m;
-var Calculo2 = salario * 0.15m;
-var Calculo3 = salario * 0.225m;
-var Calculo4 = salario * 0.275m;
-
-
-
-if (salario <= 2259.20m)
-    Console.WriteLine("você está isento do IRPF");
-
-else if (salario >= 2259.21m && salario <= 2826.65m)
+try
 {
-    Console.WriteLine($"você terá o valor de R${Calculo1} descontado do seu salário, referente aos 7,5% da alíquota, que ficará {salario} - {Calculo1}");
-    var resultado_calculo1 = salario - Calculo1;
-    Console.WriteLine($"seu salário líquido é {resultado_calculo1}");
-}
+    Console.WriteLine("Informe seu nome: ");
+    string nome = Console.ReadLine();
+    Console.WriteLine("Informe seu salário: ");
+    decimal salario = decimal.Parse(Console.ReadLine());
+    Console.WriteLine("Seja bem-vinda(o) {0}, o salário informado foi {1:C3}", nome, salario);
 
-else if (salario >= 2826.66m && salario <= 3751.05m)
+    decimal irpfDevido;
+    if (salario <= 2259.20m)
+        irpfDevido = 0;
+    else if (salario <= 2826.65m)
+        irpfDevido = salario * 0.075m - 169.21m;
+    else if (salario <= 3751.05m)
+        irpfDevido = salario * 0.15m - 381.44m;
+    else if (salario <= 4664.68m)
+        irpfDevido = salario * 0.225m - 662.77m;
+    else
+        irpfDevido = salario * 0.275m - 896m;
+
+    Console.WriteLine($"Para o salário de {salario} o valor a ser pago de IRPF é {irpfDevido}, então o salário com desconto de IRPF será {salario - irpfDevido}");
+
+}
+catch(FormatException)
 {
-    Console.WriteLine($"você terá o valor de R${Calculo2} descontado do seu salário, referente aos 15% da alíquota, que ficará {salario} - {Calculo2}");
-    var resultado_calculo2 = salario - Calculo2;
-    Console.WriteLine($"seu salário líquido é {resultado_calculo2}");
+    Console.WriteLine("O salário deve ser informado em formato numérico.");
+}
+catch (Exception e)
+{   
+    Console.WriteLine("Ocorreu um erro. Contate o suporte técnico.");
+    Console.WriteLine(e.StackTrace);
 }
 
-else if (salario >= 3751.06m && salario <= 4664.68m)
-{
-    Console.WriteLine($"Você terá o valor de R${Calculo3} descontado do seu salário, referente aos 22,5% da alíquota, que ficará {salario} - {Calculo3}");
-    var resultado_calculo3 = salario - Calculo3;
-    Console.WriteLine($"seu salário líquido é {resultado_calculo3}");
-}
-
-else
-{ 
-    Console.WriteLine($"Você terá o valor de {Calculo4} descontado do seu salário, referente aos 27,5% da alíquota, que ficará {salario} - {Calculo4}");
-    var resultado_calculo4 = salario - Calculo4;
-    Console.WriteLine($"seu salário líquido será {resultado_calculo4}");
-}
